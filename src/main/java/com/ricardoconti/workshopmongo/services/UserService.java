69e689aba1d76ab05c101caffe0551036e786b7a
Ricardo Conti <ricardoconti2718@gmail.com>
2021-08-03 18:29:57 -0300

    Obtendo um usuario por id

--- src/main/java/com/ricardoconti/workshopmongo/services/UserService.java
+++ src/main/java/com/ricardoconti/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.ricardoconti.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ricardoconti.workshopmongo.domain.User;
 import com.ricardoconti.workshopmongo.repository.UserRepository;
+import com.ricardoconti.workshopmongo.services.exception.ObjectNotFoundExcepetion;
 
 @Service
 //Serviço responsável por trabalhar com usuários
@@ -20,6 +22,11 @@ public class UserService {
 	//Método responsável por retornar todos os usuários do banco
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById (String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() ->  new ObjectNotFoundExcepetion("Objeto não encontrado"));
 	}
 
 }
---
+++ src/main/java/com/ricardoconti/workshopmongo/services/exception/ObjectNotFoundExcepetion.java
@@ -0,0 +1,10 @@
+package com.ricardoconti.workshopmongo.services.exception;
+
+public class ObjectNotFoundExcepetion extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundExcepetion(String msg) {
+		super(msg);
+	}
+	
+}
