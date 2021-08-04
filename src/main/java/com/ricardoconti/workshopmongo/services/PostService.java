211b146260de6901d75dedd288a9aae93ea67b93
Ricardo Conti <ricardoconti2718@gmail.com>
2021-08-04 11:50:24 -0300

    Obtendo um post por id

---
+++ src/main/java/com/ricardoconti/workshopmongo/services/PostService.java
@@ -0,0 +1,31 @@
+package com.ricardoconti.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ricardoconti.workshopmongo.domain.Post;
+import com.ricardoconti.workshopmongo.domain.User;
+import com.ricardoconti.workshopmongo.repository.PostRepository;
+import com.ricardoconti.workshopmongo.services.exception.ObjectNotFoundExcepetion;
+
+@Service
+//Serviço responsável por trabalhar com usuários
+public class PostService {
+	
+	//Para realizar a listagem dos usuários, a camada de serviço tem que conversar com o repository
+	//A anotação serve para instanciar automaticamente o objeto no serviço, injeção de depedencia
+	@Autowired
+	private PostRepository repo;
+	
+
+	public Post findById (String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() ->  new ObjectNotFoundExcepetion("Objeto não encontrado"));
+	}
+	
+
+	
+
+}
